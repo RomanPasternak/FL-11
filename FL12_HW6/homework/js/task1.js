2c0064a49d74a3196ae6f921b324e881c31be261
Roman Pasternak <38011782+RomanPasternak@users.noreply.github.com>
2019-12-24 20:14:59 +0200

    Add files via upload

---
+++ FL12_HW6/homework/js/task1.js
@@ -0,0 +1,38 @@
+alert('Input 3 values (a, b, c) for quadratic equation (ax2 + bx + c = 0)')
+let a, b, c, x1, x2, d;
+let obj = {
+    'a': '',
+    'b': '',
+    'c': ''
+};
+
+for(let k = 0; k < Object.keys(obj).length; k++){
+    let iter;
+    while(!isFinite(iter)){ 
+        if(iter !== undefined){
+            console.log('Invalid input data');
+        } 
+        iter = prompt('Input ' + Object.keys(obj)[k]);
+        if(Object.keys(obj)[k] === 'a' && iter === '0' || iter === null || iter === '' || isNaN(parseFloat(iter))){
+            iter = 'aaa';
+        }
+    } 
+    obj[Object.keys(obj)[k]] = parseFloat(iter);
+}
+
+a = obj['a'];
+b = obj['b'];
+c = obj['c'];
+
+d = Math.pow(b,2) - 4 * a * c; 
+
+if(d < 0){
+    console.log('no solution');
+}else if(d > 0){
+    x1 = (-b + Math.sqrt(d))/(2*a);
+    x2 = (-b - Math.sqrt(d))/(2*a);
+    console.log('x1 = '+ Math.floor(x1) + ' and x2 = ' + Math.floor(x2));
+}else if(d === 0){
+    x1 = -b/(2*a);
+    console.log('x = '+ Math.floor(x1) );
+}
