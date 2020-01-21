7fe81609272afb66af3336ac68a3101c531fc234
Roman Pasternak <38011782+RomanPasternak@users.noreply.github.com>
2020-01-21 23:50:15 +0200

    Update fighters_game.js

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -1,1 +1,14 @@
+const myFighter = new Fighter({name: ‘Maximus, damage: 25, hp: 100, strength: 30, agility: 25}); 
+class Fighter {
 
+  constructor(name, damage, hp, strength, agility) {
+    this.name = name;
+    this.damage = damage;
+    this.hp = hp;
+    this.strength = strength;
+    this.agility = agility;
+  }
+  getName(){
+    console.log(this.name);
+  }
+}
